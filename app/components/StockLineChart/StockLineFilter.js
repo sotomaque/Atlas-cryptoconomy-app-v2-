4dcdfcc383dd76fdc62601ccdac5a8da0a8a0954
Qian <qianwang1013@hotmail.com>
2018-02-23 22:48:57 -0500

    connect filter with action

--- app/components/StockLineChart/StockLineFilter.js
+++ app/components/StockLineChart/StockLineFilter.js
@@ -1,49 +1,59 @@
-import React, { Component } from "react";
-import { View, Button, Text } from "react-native";
-import { StockLineFilterStyles } from "./styles";
-import { updateFilter } from '../../actions/index.js';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
+import { StockLineFilterStyles } from './styles';
+import { sendChartData } from '../../actions';
+import cryptoApi from '../../../app/lib/crypto-compare-api';
+
 class StockLineFilter extends Component {
-	
+
 	constructor(props){
 		super(props);
 	}
 
 	/***
 	 *	@Param : option
-	 * 
+	 *
 	 */
-	onFilterStockChart( option ){
-		this.props.onSelectFilterClick(option);
-		
+	onFilterStockChart(option) {
+		cryptoApi.getHistoricalData({ ...option,
+			coinName: 'BTC' })
+			.then((res) => {
+				this.props.sendChartData(res);
+			});
 	}//onFilterStockChart
-	
+
     render() {
         const { container, text } = StockLineFilterStyles;
 
         return (
             <View style={container}>
-            	<View>
-		            <Text onPress={() => this.onFilterStockChart({filter:'DAY'})} style={text}>1D</Text>
-		        </View>
-		        <View>
-		            <Text  onPress={() => this.onFilterStockChart({filter:'WEEK'})} style={text}>1W</Text>
-		        </View>
-		        <View>
-		            <Text  onPress={() => this.onFilterStockChart({filter:'MONTH'})} style={text}>1M</Text>
-		        </View>
-		        <View>
-		            <Text  onPress={() => this.onFilterStockChart({filter:'3MONTH'})} style={text}>3M</Text>
-		        </View>
-		        <View>
-		            <Text  onPress={() => this.onFilterStockChart({filter:'6MONTH'})} style={text}>6M</Text>
-		        </View>
-		        <View>
-		            <Text  onPress={() => this.onFilterStockChart({filter:'1YEAR'})} style={text}>1Y</Text>
-		        </View>
+						<View>
+						<Text onPress={() => this.onFilterStockChart({ filter: 'DAY' })} style={text}>1D</Text>
+						</View>
+						<View>
+						<Text onPress={() => this.onFilterStockChart({ filter: 'WEEK' })} style={text}>1W</Text>
+						</View>
+						<View>
+						<Text onPress={() => this.onFilterStockChart({ filter: 'MONTH' })} style={text}>1M</Text>
+						</View>
+						<View>
+						<Text onPress={() => this.onFilterStockChart({ filter: '3MONTH' })} style={text}>3M</Text>
+						</View>
+						<View>
+						<Text onPress={() => this.onFilterStockChart({ filter: '6MONTH' })} style={text}>6M</Text>
+						</View>
+						<View>
+						<Text onPress={() => this.onFilterStockChart({ filter: '1YEAR' })} style={text}>1Y</Text>
+						</View>
                 <View>
-                    <Text style={text}>MAX</Text>
+                    <Text
+											onPress={() => this.onFilterStockChart({ filter: 'MAX' })}
+											style={text}
+                    >
+											MAX
+										</Text>
                 </View>
             </View>
         );
@@ -51,16 +61,15 @@ class StockLineFilter extends Component {
 }
 
 
-	
-export default connect (
-	(store) => ({
-		filter : store.stockFilterReducer.filter
-	})
-	,(dispatch) =>{
-		return {
-			onSelectFilterClick : option => {
-				dispatch(updateFilter(option))
-			}
-		}
-	})
-(StockLineFilter)
+function mapStateToProps(store) {
+  return {
+		filter: store.stockFilterReducer.filter,
+		stockList: store.stockFilterReducer.stockList,
+		stockData: store.stockFilterReducer.stockData,
+		selectedPoint: store.stockFilterReducer.selectedPoint,
+		endPoint: store.stockFilterReducer.endPoint
+  };
+}
+
+
+export default connect(mapStateToProps,	{ sendChartData })(StockLineFilter)
--- app/lib/crypto-compare-api.js
+++ app/lib/crypto-compare-api.js
@@ -2,25 +2,25 @@
 var cryptoApi = {
 
 	// define constants
-	DEFAULT_ENDPOINT 			: 'https://min-api.cryptocompare.com/data/'
-	,HISTORICAL_DATA 			: 'histo/@FILTER/?fsym=/@COIN_NAME/&tsym=USD&limit=/@LIMIT/&aggregate=/@AGGREGATE/'
-	,NUM_DATA_POINTS			: 100
+	DEFAULT_ENDPOINT: 'https://min-api.cryptocompare.com/data/'
+	,HISTORICAL_DATA: 'histo/@FILTER/?fsym=/@COIN_NAME/&tsym=USD&limit=/@LIMIT/&aggregate=/@AGGREGATE/'
+	,NUM_DATA_POINTS: 2000
 
 	// public calls
 	,getHistoricalData	: function( option ){
-		
+
 		console.log('getHistoricalData', option);
-		
+
 		if( !( option.coinName && option.filter ) ){
 			console.error('Coin Name and Filter required')
 			return;
 		}
-		
+
 		var filtr 	= ''
 		,aggreate	= ''
 		,coinName	= option.coinName
 		,limit		= option.limit || cryptoApi.NUM_DATA_POINTS
-		
+
 		switch(option.filter){
 			case 'DAY' :
 				filter 		= 	'minute'
@@ -28,30 +28,40 @@ var cryptoApi = {
 				,limit		=	'200'
 				break;
 			case 'WEEK':
-				filter		=	'day'
-				,aggreate	= 	'7'
+				filter		=	'hour'
+				,aggreate	= 	'1'
+				,limit		=	'168'
 				break;
-			case 'MONTH' :	
-				filter 		=	'day'
-				,aggreate	= 	'30'
+			case 'MONTH' :
+				filter 		=	'hour'
+				,aggreate	= 	'3'
+				,limit		=	'243'
 				break;
 			case '3MONTH':
-				filter		= 	'day'
-				,aggreate	= 	'90'
+				filter		= 	'hour'
+				,aggreate	= 	'9'
+				,limit		=	'243'
 				break;
 			case '6MONTH':
 				filter		= 	'day'
-				,aggreate	= 	'180'
+				,aggreate	= 	'1'
+				,limit		=		'180'
 				break;
-			case '1YEAR':	
+			case '1YEAR':
 				filter		=	'day'
-				,aggreate	=	'365'
+				,aggreate	=	'1'
+				,limit		=	'365'
 				break;
-			default	: 
+			case 'MAX':
+				filter		= 'day'
+				,aggreate =	'10'
+				,allData	=	'true'
+				break;
+			default	:
 				console.error('Filter not defined');
-				return; 
-		}	
-		
+				return;
+		}
+
 
 		var finalUrl 	= cryptoApi.HISTORICAL_DATA.replace('/@FILTER/'	, filter)
 												.replace('/@COIN_NAME/'	, coinName)
@@ -67,16 +77,16 @@ var cryptoApi = {
 			return cryptoApi.trimDataSetToList(res.Data)
 		})
 	}//getHistoricalData
-	
+
 	,trimDataSetToList		: function(arr){
-		
+
 		console.log('trimDataSetToList', arr);
-		
+
 		return arr.map(function( dataPoint ){
-			return dataPoint.close 
+			return dataPoint.close
 		})
 	}// trimDataSet
-	
-}  
+
+}
 
 module.exports = cryptoApi;
