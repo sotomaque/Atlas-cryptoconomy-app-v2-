956405929be2fa644b9661242eec85a4638754fd
Qian <qianwang1013@hotmail.com>
2018-03-03 19:26:12 -0500

    adding more portfolio endpoint

--- app/lib/rest-api.js
+++ app/lib/rest-api.js
@@ -10,18 +10,18 @@ const restApi = {
 
   // saveTransaction
   saveTransaction(transactions, callback) {
-    transactions = {
-      userId: '123',
-      txnCoin: 'BTC',
-      txnExchange: 'Coinbase',
-      txnCurrency: 'USD',
-      txnAmount: 1000,
-      txnQty: 1,
-      coinCurrentPrice: 15000,
-      txnType: 'Buy',
-      txnDate: '2018-02-18T00:00:00.000Z',
-      txnNotes: 'Buy more coins',
-    };
+    // transactions = {
+    //   userId: '123',
+    //   txnCoin: 'BTC',
+    //   txnExchange: 'Coinbase',
+    //   txnCurrency: 'USD',
+    //   txnAmount: 1000,
+    //   txnQty: 1,
+    //   coinCurrentPrice: 15000,
+    //   txnType: 'Buy',
+    //   txnDate: '2018-02-18T00:00:00.000Z',
+    //   txnNotes: 'Buy more coins',
+    // };
     if (!transactions) {
       return;
     }
@@ -29,6 +29,41 @@ const restApi = {
     restApi.doPost(
       restApi.LOCAL_DEFAULT_URL + restApi.TRANSACTION_SERVICE
       , transactions
+      , callback,
+    );
+  },
+
+  // createPortfolio
+  createPortfolio(userId, callback) {
+    if (!userId) {
+      return;
+    }
+
+    restApi.doPost(
+      restApi.LOCAL_DEFAULT_URL + restApi.PORTFOLIO_SERVICE
+      , userId
+      , callback,
+    );
+  },
+
+  // createPortfolio
+  getPortfolio(userId, callback) {
+    if (!userId) {
+      return;
+    }
+    restApi.doGet(
+      `${restApi.LOCAL_DEFAULT_URL + restApi.PORTFOLIO_SERVICE}/${userId}`
+      , callback,
+    );
+  },
+
+  // createPortfolio
+  updatePortfolio(portfolio, callback) {
+    if (!portfolio) {
+      return;
+    }
+    restApi.doPut(
+      restApi.LOCAL_DEFAULT_URL + restApi.PORTFOLIO_SERVICE
       , callback,
     );
   },
@@ -53,6 +88,49 @@ const restApi = {
       console.error(err);
     });
   },
+
+  doGet(url, callback) {
+    console.log(url);
+    fetch(url, {
+      headers: {
+           'content-type': 'application/json',
+       },
+       method: 'GET', // *GET, PUT, DELETE, etc.
+    }).then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      if (typeof (callback) === 'function') {
+        callback(res);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  },
+
+  doPut(url, payload, callback) {
+    console.log(url, payload);
+    fetch(url, {
+      body: JSON.stringify(payload),
+      headers: {
+           'content-type': 'application/json',
+       },
+       method: 'PUT', // *GET, PUT, DELETE, etc.
+    }).then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      if (typeof (callback) === 'function') {
+        callback(res);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  },
+
+
 };
 
 module.exports = restApi;
